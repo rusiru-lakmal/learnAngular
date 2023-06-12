64c065a3e3b3a1e283e38cb3e94d9a31e45ef17f
rusiru-lakmal <rusiru.lakmal2615@gmail.com>
2023-06-12 21:56:46 +0530

    learn built-in directives *ngIf and *ngFor

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent {
+
+  isvisible = false;
+
+  rooms:Room = {
+    totalRooms : 10,
+    availableRooms : 5,
+    bookedRooms : 5,
+  }
+
+  roomList : RoomList[] = [
+    {
+      roomNumber : 101,
+      roomType : 'Single',
+      roomPrice : 1000,
+      roomStatus : 'Available',
+      roomImage : 'assets/images/room1.jpg',
+      roomDescription : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+    },
+    {
+      roomNumber : 102,
+      roomType : 'Double',
+      roomPrice : 2000,
+      roomStatus : 'Available',
+      roomImage : 'assets/images/room2.jpg',
+      roomDescription : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+    },
+    {
+      roomNumber : 103,
+      roomType : 'Single',
+      roomPrice : 1000,
+      roomStatus : 'Available',
+      roomImage : 'assets/images/room3.jpg',
+      roomDescription : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.'
+    },
+  ]
+
+  onClick(){
+
+     this.isvisible = !this.isvisible;
+  }
+
+}
---
+++ src/app/rooms/rooms.ts
@@ -0,0 +1,14 @@
+export interface Room {
+    totalRooms : number,
+    availableRooms : number,
+    bookedRooms : number,
+}
+
+export interface RoomList {
+    roomNumber : number,
+    roomType : string,
+    roomPrice : number,
+    roomStatus : string,
+    roomImage : string,
+    roomDescription : string,
+}
